38b11492c5f21a7f86b24c5c618990705b615f01
Hugo Silva <hugofsilvaa@gmail.com>
2021-04-19 21:12:48 +0100

    home page draft

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
+    <div className='home-container'>
+      <div className='presentation'>
+        <h1>Hello World!</h1>
+        <h2>My name is Hugo Silva, a Junior Front-End Web Developer</h2>
+        <p>Passionate about innovation and technologies</p>
+        <p>So last year, in the middle of Covid pandemic,</p>
+        <p>I have decided to start a Front-End Web Developer</p>
+        <p>I am fluent in JavaScript and React</p>
+        <p>
+          My main focus now is to develop a deeper knowledge on the languages
+          having in mind the best practcises
+        </p>
+        <p>and applying that knowledge on personal and professional projects</p>
+      </div>
+      <div className='coding-gif'>
+        <img
+          src='https://media.giphy.com/media/WTjXuYA2y4o3UZly3W/giphy.gif'
+          alt='coding GIF'
+        />
+      </div>
     </div>
   );
 }
