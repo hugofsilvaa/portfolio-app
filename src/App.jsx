0225118e093f766e5d3e9109a877855194f781ce
Hugo Silva <hugofsilvaa@gmail.com>
2021-04-21 21:03:48 +0100

    projects page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Navbar from "./components/NavBar/Navbar";
+import Projects from "./components/Projects/Projects";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
+          <Route path='/projects' component={Projects} />
         </Switch>
         <Footer />
       </Router>
---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Projects.css";
+import { Link } from "react-router-dom";
+
+export default function ProjectCard({ project }) {
+  return (
+    <div className='main-container'>
+      <div className='card-container'>
+        <h1>{project.header}</h1>
+        <div className='card-photo'>
+          <img src={project.img} alt='' />
+        </div>
+        <p>{project.description}</p>
+      </div>
+      <div className='button'>
+        <Link to={{ pathname: project.link }} target='_blank'>
+          <button>Project</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
